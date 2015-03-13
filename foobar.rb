f80ace1af8f4cb442d7d396ced852105810b38b3
Bryan Wegman <bryanwegman@gmail.com>
2015-03-13 12:15:56 -0700

    Update foobar.rb to accept user input
    This version prompts the user to input the maximum value, instead of hard-coding the value in the script.

--- foobar.rb
+++ foobar.rb
@@ -1,15 +1,21 @@
 # The classic Foobar (or FizzBuzz) challenge…now in Ruby!
 
-number_of_elements = 50
+print "What number would you like me to FooBar to? > "
+number_of_elements = gets.chomp.to_i
 
-(1..number_of_elements).each do |i|
-  if i % 3 == 0 && i % 5 == 0
-    puts "Foobar"
-  elsif i % 3 == 0
-    puts "Foo"
-  elsif i % 5 == 0
-    puts "Bar"
-  else
-    puts i
-  end
+if number_of_elements > 1000000
+	print "That might take a while. Please try a number less than 1,000,000. \nWhat number would you like me to FooBar to? >"
+	number_of_elements = gets.chomp.to_i
+else
+	(1..number_of_elements).each do |i|
+	  if i % 3 == 0 && i % 5 == 0
+	    puts "Foobar"
+	  elsif i % 3 == 0
+	    puts "Foo"
+	  elsif i % 5 == 0
+	    puts "Bar"
+	  else
+	    puts i
+	  end
+	end	  
 end
