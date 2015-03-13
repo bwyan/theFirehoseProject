dbaa6f9809454ccb5694a54bb22a6ba99a282283
Bryan Wegman <bryanwegman@gmail.com>
2015-03-13 12:28:34 -0700

    Fix foobar.rb error state
    This version corrects a bug in the error handling if a user input a number greater than 1,000,000. The previous version did not rerun the foobar program if a user didn't input a valid number on the first try.

--- foobar.rb
+++ foobar.rb
@@ -1,21 +1,30 @@
 # The classic Foobar (or FizzBuzz) challenge…now in Ruby!
 
+def foobar(max_value)
+	if max_value > 1000000
+		print "That might take a while. Please try a number less than 1,000,000. \nWhat number would you like me to FooBar to? >"
+		number_of_elements = gets.chomp.to_i
+		foobar(number_of_elements)
+	else
+		(1..max_value).each do |i|
+		  if i % 3 == 0 && i % 5 == 0
+		    puts "Foobar"
+		  elsif i % 3 == 0
+		    puts "Foo"
+		  elsif i % 5 == 0
+		    puts "Bar"
+		  else
+		    puts i
+		  end
+		end	  
+	end
+end
+
 print "What number would you like me to FooBar to? > "
 number_of_elements = gets.chomp.to_i
+foobar(number_of_elements)
 
-if number_of_elements > 1000000
-	print "That might take a while. Please try a number less than 1,000,000. \nWhat number would you like me to FooBar to? >"
-	number_of_elements = gets.chomp.to_i
-else
-	(1..number_of_elements).each do |i|
-	  if i % 3 == 0 && i % 5 == 0
-	    puts "Foobar"
-	  elsif i % 3 == 0
-	    puts "Foo"
-	  elsif i % 5 == 0
-	    puts "Bar"
-	  else
-	    puts i
-	  end
-	end	  
-end
+
+
+
+
